8fc3c187a442b0acc4741a430a2da01c7129b143
Wida3tk <ro7e.entaa@gmail.com>
2026-05-04 01:51:50 +0500

    fix i18n files

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,23 +1,6 @@
-// src/i18n/routing.ts
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['ar', 'en'],
   defaultLocale: 'ar',
-});
-
-// src/i18n/request.ts
-import { getRequestConfig } from 'next-intl/server';
-import { routing } from './routing';
-
-export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
-  if (!locale || !routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale;
-  }
-
-  return {
-    locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
-  };
 });
